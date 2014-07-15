845f3286340c53604d17668d587e0a15f476da5b
pramod <pramod@tavisca.com>
2014-07-15 15:44:42 +0530

    Bowling-Game problem | added test case for all spare to the project

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -8,14 +8,96 @@ namespace Bowling
 {
     public class Game
     {
+        List<Roll> ScorePerRoll = new List<Roll>();
+      
+        private int score = 0;
+        private int currentRollIndex = 1;
+        private int rollCount = 0;
+
+        private int strike = 0;
+        private int spare = 0;
+
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            ScorePerRoll.Add(GetRoll(pins));
+          
+            UpdateScore();
+        }
+
+        private Roll GetRoll(int pins)
+        {
+            Roll currentRoll = new Roll();
+
+            currentRoll.RollScore = pins;
+
+            if (strike > 0 || spare > 0)
+            {
+                currentRoll.Bonus = true;
+                if (spare > 0)
+                    spare--;
+
+                UpdateSpareStrikeCounter(pins, currentRoll);
+            }
+            else 
+            {
+                UpdateSpareStrikeCounter(pins, currentRoll);
+            }
+            currentRollIndex++;
+            rollCount++;
+            return currentRoll;
+        }
+
+        private void UpdateSpareStrikeCounter(int pins, Roll currentRoll)
+        {
+            if (rollCount == 0 || rollCount % 2 == 0)
+            {
+                if (pins == 10 && rollCount < 10)
+                {
+                    strike += 2;
+                    rollCount++;
+                }
+            }
+            else
+            {
+                if (rollCount % 2 == 1 && rollCount < 10)
+                {
+                    int frameScore = currentRoll.RollScore
+                            + ScorePerRoll[currentRollIndex - 2].RollScore;
+                    if (frameScore == 10)
+                    {
+                        spare = 1;
+                    }
+                }
+            }
+        }
+
+        private void UpdateScore()
+        {
+            score += ScorePerRoll[ScorePerRoll.Count - 1].RollScore;
+            
+            if (ScorePerRoll[ScorePerRoll.Count - 1].Bonus)
+            {
+
+                if (strike <= 2)
+                {
+                    score += ScorePerRoll[ScorePerRoll.Count - 1].RollScore;
+                    strike--;
+                }
+             
+                while (strike > 2)
+                {
+                    score += ScorePerRoll[ScorePerRoll.Count - 1].RollScore*2 ;
+                    strike--;
+                }
+            }
+            
+
         }
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            return score;
         }
 
     }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -1,14 +1,84 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bowling;
 
 namespace BowlingFixtures
 {
     [TestClass]
     public class GameFixture
     {
-        [TestMethod]
-        public void TestMethod1()
+        public Game game;
+        public int strike = 0;
+        public int Spare = 0;
+
+        // roll(int pins)
+        // update roll list 
+        // get the current score and add the current roll
+        
+        [TestInitialize]
+        public void init()
         {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void CheckUpdatedScoreAfterFirstRoll()
+        {
+            int numberOfPinsHit = 4;
+
+            game.Roll(numberOfPinsHit);
+
+            Assert.AreEqual(4,game.GetScore());
+        }
+
+        [TestMethod]
+        public void CheckUpdatedScoreAfterSecondRoll()
+        {
+            int numberOfPinsHitOnFirstRoll = 4;
+            int numberOfPinsHitOnSecondRoll = 3;
+            game.Roll(numberOfPinsHitOnFirstRoll);
+            game.Roll(numberOfPinsHitOnSecondRoll);
+
+            Assert.AreEqual(7, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CheckUpdatedScoresForSpare()
+        {
+            int numberOfPinsHitOnFirstRoll = 4;
+            int numberOfPinsHitOnSecondRoll = 6;
+            int numberOfPinsHitOnThirdRoll = 3;
+            game.Roll(numberOfPinsHitOnFirstRoll);
+            game.Roll(numberOfPinsHitOnSecondRoll);
+            game.Roll(numberOfPinsHitOnThirdRoll);
+
+            Assert.AreEqual(16, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CheckUpdatedScoresForStrike()
+        {
+            int numberOfPinsHitOnFirstRoll = 10;
+            int numberOfPinsHitOnSecondRoll = 6;
+            int numberOfPinsHitOnThirdRoll = 3;
+            game.Roll(numberOfPinsHitOnFirstRoll);
+            game.Roll(numberOfPinsHitOnSecondRoll);
+            game.Roll(numberOfPinsHitOnThirdRoll);
+
+            Assert.AreEqual(28, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CheckUpdatedScoresForAllStrike()
+        {
+            int numberOfPinsHitOnFirstRoll = 10;
+           
+            for(int i=1;i <= 12; i++)
+            {
+                game.Roll(numberOfPinsHitOnFirstRoll);
+            }
+            int result =game.GetScore();
+            Assert.AreEqual(300, game.GetScore());
         }
     }
 }
