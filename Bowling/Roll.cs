4d59794629e5d1ed6ae90ee0d108dbad4db676e1
pramod <pramod@tavisca.com>
2014-07-15 15:46:24 +0530

    Bowling-Game problem | added all updated files to the project

---
+++ Bowling/Roll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public class Roll
+    {
+        public int RollScore{ get; set;}
+
+        public bool Bonus { get; set; }
+    }
+}
